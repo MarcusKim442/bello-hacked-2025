7e0883fc05519f9498eb8f1ff1cc757c12b20792
Taehun Lee <91559425+Hentyphoon@users.noreply.github.com>
2025-02-15 13:22:07 -0700

    Update popup.js

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,18 +1,27 @@
-console.log("Extension is currently running");
+alert ("Extension is currently running");
 
 function fetch_input(Input_id){
     let input = document.getElementById(Input_id);
     if (input){
-        return input;
+        return input.value;
     } else {
         console.error("No input Found");
         return null;
     }
 }
+function send_to_tool(text){
+    
+}
 
-let input_text = fetch_input(userInput);
 
 
-
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("check");
+    button.addEventListener("click", () => {
+        let input_text = fetch_input("userInput"); 
+        send_to_tool(input_text); 
+    });
+    
+});
 
 
