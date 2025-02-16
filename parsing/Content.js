bb7433a57030dd7a16a7a2e74378092a6c0f8390
Marcus <marcuskim442@gmail.com>
2025-02-16 11:35:57 -0700

    Revert "dctfvgyb"
    This reverts commit 08babca4c19c24f963228b1cdd3d5ff009d29cd9.

--- parsing/Content.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("mouseup", function () {
-    let selectedText = window.getSelection().toString().trim();
-    
-    if (selectedText.length > 5) {
-        // Send the selected text to background.js
-        chrome.runtime.sendMessage({ action: "extractClaims", text: selectedText });
-    }
-});
